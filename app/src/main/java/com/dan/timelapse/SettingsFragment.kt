83c101dbf3b1b9ab350395312b2121e574bdbbce
Dan Oprea <dan.oprea@masternaut.com>
2023-01-24 09:38:01 +0100

    Update Settings dialog.

--- app/src/main/java/com/dan/timelapse/SettingsFragment.kt
+++ app/src/main/java/com/dan/timelapse/SettingsFragment.kt
@@ -17,6 +17,12 @@ class SettingsFragment(activity: MainActivity ) : AppFragment(activity) {
 
     private lateinit var binding: SettingsFragmentBinding
 
+    private fun update() {
+        binding.switchH265.text = if (binding.switchH265.isChecked) "Encoder H.265 (HEVC)" else "Encoder H.264 (old)"
+        binding.switchCrop.text = if (binding.switchCrop.isChecked) "Crop images" else "Don't crop (back borders)"
+        binding.switch4K.text = if (binding.switch4K.isChecked) "Video 4K" else "Video 1080p"
+    }
+
     override fun onBack(homeButton: Boolean) {
         settings.h265 = binding.switchH265.isChecked
         settings.crop = binding.switchCrop.isChecked
@@ -31,6 +37,12 @@ class SettingsFragment(activity: MainActivity ) : AppFragment(activity) {
         binding.switchH265.isChecked = settings.h265
         binding.switchCrop.isChecked = settings.crop
         binding.switch4K.isChecked = settings.encode4K
+
+        binding.switchH265.setOnCheckedChangeListener { _, _ -> update()  }
+        binding.switchCrop.setOnCheckedChangeListener { _, _ -> update()  }
+        binding.switch4K.setOnCheckedChangeListener { _, _ -> update()  }
+
+        update()
 
         return binding.root
     }
