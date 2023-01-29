c6a28c0d55782945611e5eb65f4681ed78016581
Dan <doprea@gmail.com>
2023-01-29 10:04:53 +0100

    Convert AverageFramesFilter to MultiFramesFilter

--- app/src/main/java/com/dan/timelapse/AverageFramesFilter.kt
+++ app/src/main/java/com/dan/timelapse/AverageFramesFilter.kt
@@ -5,47 +5,29 @@ import org.opencv.core.CvType.CV_16UC3
 import org.opencv.core.CvType.CV_8UC3
 import org.opencv.core.Mat
 
-class AverageFramesFilter(private val size: Int, nextConsumer: FramesConsumer) : FramesFilter(nextConsumer) {
-    private val frames = mutableListOf<Mat>()
+class AverageFramesFilter(private val size: Int, nextConsumer: FramesConsumer)
+    : MultiFramesFilter(size, true, nextConsumer) {
     private val outputFrame = Mat()
     private val sum = Mat()
-    private var counter = 0
-
-    override fun startFilter() {
-        for (index in 1 .. size) {
-            frames.add(Mat())
-        }
-
-        counter = 0
-
-        super.startFilter()
-    }
 
     override fun stopFilter() {
         sum.release()
         outputFrame.release()
-        frames.clear()
         super.stopFilter()
     }
 
-    override fun consume(frame: Mat) {
-        val index = counter % size
-        counter++
-
-        val prevMat = frames[index]
-        frames[index] = frame.clone()
-
+    override fun consume(removedFrame: Mat?, lastFrame: Mat?, newFrame: Mat, allFrames: Array<Mat?>) {
         if (sum.empty()) {
-            frame.convertTo(sum, CV_16UC3)
+            newFrame.convertTo(sum, CV_16UC3)
         } else {
-            Core.add( sum, frame, sum, Mat(), CV_16UC3 )
+            Core.add( sum, newFrame, sum, Mat(), CV_16UC3 )
         }
 
-        if (!prevMat.empty()) {
-            Core.subtract( sum, prevMat, sum, Mat(), CV_16UC3 )
+        if (null != removedFrame) {
+            Core.subtract( sum, removedFrame, sum, Mat(), CV_16UC3 )
         }
 
-        if (counter < size) return
+        if (null == lastFrame) return
 
         sum.convertTo( outputFrame, CV_8UC3, 1.0 / size )
         nextConsumer.consume(outputFrame)
