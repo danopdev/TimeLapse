e145d8459b08c196e3eec760a31dec11983c80f1
Dan <doprea@gmail.com>
2023-01-29 10:00:00 +0100

    Convert TransitionFramesFilter to MultiFramesFilter

--- app/src/main/java/com/dan/timelapse/TransitionFramesFilter.kt
+++ app/src/main/java/com/dan/timelapse/TransitionFramesFilter.kt
@@ -5,32 +5,30 @@ import org.opencv.core.CvType.CV_16UC3
 import org.opencv.core.CvType.CV_8UC3
 import org.opencv.core.Mat
 
-class TransitionFramesFilter(private val size: Int, nextConsumer: FramesConsumer) : FramesFilter(nextConsumer) {
-    private var prevFrame = Mat()
+class TransitionFramesFilter(private val size: Int, nextConsumer: FramesConsumer)
+    : MultiFramesFilter(2, true, nextConsumer) {
     private val tmpFrame1 = Mat()
     private val tmpFrame2 = Mat()
     private val outputFrame = Mat()
 
     override fun stopFilter() {
-        prevFrame.release()
         tmpFrame1.release()
         tmpFrame2.release()
         outputFrame.release()
         super.stopFilter()
     }
 
-    override fun consume(frame: Mat) {
-        if (!prevFrame.empty()) {
+    override fun consume(removedFrame: Mat?, lastFrame: Mat?, newFrame: Mat, allFrames: Array<Mat?>) {
+        if (null != lastFrame) {
             for(step in 1 until size) {
-                prevFrame.convertTo(tmpFrame1, CV_16UC3, (size - step).toDouble())
-                frame.convertTo(tmpFrame2, CV_16UC3, step.toDouble())
+                lastFrame.convertTo(tmpFrame1, CV_16UC3, (size - step).toDouble())
+                newFrame.convertTo(tmpFrame2, CV_16UC3, step.toDouble())
                 Core.add( tmpFrame1, tmpFrame2, tmpFrame1 )
                 tmpFrame1.convertTo(outputFrame, CV_8UC3, 1.0 / size.toDouble())
                 nextConsumer.consume(outputFrame)
             }
         }
 
-        prevFrame = frame.clone()
-        nextConsumer.consume(frame)
+        nextConsumer.consume(newFrame)
     }
 }
